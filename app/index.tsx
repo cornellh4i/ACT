ef7638500e91b4d968f69ce81102b031ce639b3a
Samantha Cruz <samantha.jaycruz@gmail.com>
2025-09-18 02:18:54 -0400

    Add clickable button to home

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, Pressable, View, Alert } from 'react-native';
 
 import '../global.css';
 
@@ -10,6 +10,18 @@ export default function App() {
     // Simulate it in the Expo Go app on your phone and make a PR with a screenshot!
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-semibold">ACT WHOOP!</Text>
+      <Pressable
+        onPress={() => Alert.alert("Woah Cowboy nice click!")}
+        className="flex px-[16px] py-[12px] items-center gap-[8px] bg-[#0006] active:bg-[#0009] rounded-[50px]"
+        accessibilityLabel="Totally Click this for cool cool thing"
+      >
+        <Text
+          className="text-[20px] font-semibold text-black"
+        >
+          Omg Click Me
+        </Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
