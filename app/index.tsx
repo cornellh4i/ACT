ec2b2e52cf9cbe71082755bd9aafceb7e0f6daa9
Lillian Wang <lw782@cornell.edu>
2025-10-01 20:18:49 -0400

    Created a button that increases a counter

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, Text, View } from 'react-native';
 
 import '../global.css';
 
 export default function App() {
+  const [counter, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount((counter) => counter + 1);
+  };
   return (
     // TODO: Create and style a button that does something when clicked! :)
     // Use Nativewind for styling and react-native components
@@ -11,6 +16,14 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-semibold">ACT WHOOP!</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={() => {
+          increaseCount();
+          Alert.alert('Counter', `${counter}`);
+        }}
+        title="Click to increase the counter!"
+        color="#ff0000ff"
+      />
     </View>
   );
 }
