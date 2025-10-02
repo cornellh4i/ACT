2dcf156c202e238a6c319c3e03a44031c9e5ad3d
Your Name <cjw269@cornell.edu>
2025-10-01 20:23:35 -0400

    added button

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Alert, Text, TouchableOpacity, View } from 'react-native';
 
 import '../global.css';
 
@@ -10,6 +10,9 @@ export default function App() {
     // Simulate it in the Expo Go app on your phone and make a PR with a screenshot!
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-semibold">ACT WHOOP!</Text>
+      <TouchableOpacity className="bg-pink-100" onPress={() => Alert.alert('I DID SOMETHING')}>
+        <Text>I do something</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
