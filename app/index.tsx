50d88d80b448eab7daa693ed5bc23967209adc71
omr6 <omr6@cornell.edu>
2025-10-01 20:18:35 -0400

    Created a button that increases a counter

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { useState } from 'react'
 
 import '../global.css';
 
+
 export default function App() {
+
+  const [count, setCount] = useState(0)
+  const increaseCount = () => {
+    setCount(count => count + 1)
+  }
+
   return (
     // TODO: Create and style a button that does something when clicked! :)
     // Use Nativewind for styling and react-native components
     // Simulate it in the Expo Go app on your phone and make a PR with a screenshot!
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-semibold">ACT WHOOP!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={increaseCount} title="Press Me" color="#b3cde0"/>
+      <Text>{count}</Text>
     </View>
   );
 }
